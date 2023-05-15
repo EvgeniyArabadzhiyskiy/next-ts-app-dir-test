9fe4a6469ef376f600de6f32405c26848e6417c3
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-15 22:24:39 +0300

    Create page.tsx

---
+++ app/ssr/page.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import axios from "axios";
+import React from "react";
+import { store } from "@/redux/store";
+import { setPokemonList } from "@/redux/slices/pokemonSlice";
+
+interface IPokemon {
+  name: string;
+  url: string;
+}
+
+export default async function HomePage() {
+    const res = await fetch( `https://pokeapi.co/api/v2/pokemon?offset=0&limit=10`);
+    const data = await res.json()
+    console.log("HomePage  data:", data.results);
+
+    // store.dispatch(setPokemonList(data.results))
+    console.log("HomePage  store:", store);
+
+    return (
+    <>
+      <h1>SSR PAGE</h1>
+      <Link href="/">HOME</Link>
+
+      {/* <ul>
+        {data &&
+          data.results.map((pokemon: IPokemon) => {
+            return <li key={pokemon.name}>{pokemon.name}</li>;
+          })}
+      </ul> */}
+    </>
+  );
+}
