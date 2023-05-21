97bf55eddea7d377d0dcc2c907d1497f6bf056c4
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-21 22:15:19 +0300

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,12 @@
+import LoginForm from "@/components/LoginForm/LoginForm";
 import Link from "next/link";
-
 export default function LoginPage() {
   return (
     <>
       <Link href="/">HOME</Link>
       <h1>Login Page</h1>
+
+      <LoginForm />
     </>
   );
 }
