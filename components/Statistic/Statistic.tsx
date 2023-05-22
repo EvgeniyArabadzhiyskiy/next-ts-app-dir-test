22807f28d214582cf905ce1d5c3188a08dff4515
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-22 18:27:08 +0300

    Create Statistic.tsx

---
+++ components/Statistic/Statistic.tsx
@@ -0,0 +1,14 @@
+// "use client"
+import { Title, Wrapper } from "./Statistic.styled";
+
+const Statistic = () => {
+  return (
+    <div>
+      <Title>Pokemon Statistic</Title>
+      <Wrapper>Statistic</Wrapper>
+      <h1>STATISTIC</h1>
+    </div>
+  );
+};
+
+export default Statistic;
