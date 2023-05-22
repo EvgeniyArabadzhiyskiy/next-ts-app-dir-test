f02e5f26963cbd7e631c23f740046caf70ca8d34
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-22 18:27:27 +0300

    Create LoginForm.tsx

---
+++ components/LoginForm/LoginForm.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import { useState } from "react";
+
+function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (e: any)  => {
+    e.preventDefault()
+    // console.log({email, password});
+
+    const sss = await signIn("credentials", {
+        email: email,
+        password: password,
+        redirect: true,
+        callbackUrl: '/'
+    })
+    
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="text" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Sign In</button>
+      </form>
+    </>
+  );
+}
+
+export default LoginForm;
