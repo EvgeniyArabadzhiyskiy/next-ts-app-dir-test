19f59d0667c05b0c1c51cd77c575bd22e80d6d4f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-25 22:01:30 +0300

    Create cart.tsx

---
+++ pages/cart.tsx
@@ -0,0 +1,12 @@
+import Statistic from "@/components/Statistic/Statistic";
+
+const About = () => {
+  return (
+    <>
+      <h1>About PAGE</h1>
+      <Statistic />
+    </>
+  );
+};
+
+export default About;
