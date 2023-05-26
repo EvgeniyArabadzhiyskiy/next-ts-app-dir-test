b13aa61a6fb2aab290ac86e75099095349e4b282
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-26 22:04:40 +0300

    Update LoginForm.tsx

--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -1,31 +1,97 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
+import { useSession } from "next-auth/react";
+import { redirect, usePathname } from "next/navigation";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { setCookie } from "nookies";
+
+const login = async (credentials: any) => {
+  const BASE_URL = "https://wallet-backend-xmk0.onrender.com/api";
+  const USER_LOGIN = "/users/login";
+
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(credentials),
+  };
+
+  // const resFetch = await fetch(`${BASE_URL}${USER_LOGIN}`, options);
+  // const user = await resFetch.json();
+  // console.log("login  user:", user);
+
+  const { data: user } = await axios.post(`${BASE_URL}${USER_LOGIN}`, credentials);
+
+  return user;
+};
 
 function LoginForm() {
+  // const pathname = usePathname();
+  const session = useSession();
+  const queryClient = useQueryClient();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const onSubmit = async (e: any)  => {
-    e.preventDefault()
-    // console.log({email, password});
+  const mutation = useMutation({
+    mutationFn: login,
+    onSuccess: (data) => {
+      const { token, ...rest } = data;
+      queryClient.setQueryData(["currentUser"], rest);
+      
+      setCookie(null, "authToken", `${token}`, {
+          maxAge: 30 * 24 * 60 * 60,
+          path: "/",
+        });
+        // const ddd = queryClient.getQueryData(['currentUser'])
+        // console.log("LoginForm  ddd:", ddd);
+    },
+  });
 
-    const sss = await signIn("credentials", {
-        email: email,
-        password: password,
-        // redirect: false,
-        // redirect: true,
-        // callbackUrl: '/about'
-    })
-    
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // const sss = await signIn("credentials", {
+    //   email: email,
+    //   password: password,
+    //   // redirect: false,
+    //   // redirect: true,
+    //   // callbackUrl: '/about'
+    // });
+
+    mutation.mutate({ email, password });
   };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case "email":
+        setEmail(value);
+        return;
+
+      case "password":
+        setPassword(value);
+        return;
+
+      default:
+        return;
+    }
+  };
+
+  // if (session.status === "loading") {
+  //   return <h1>Session loading ...</h1>;
+  // }
+
+  // if (session.status === "authenticated") {
+  //   // redirect("/home");
+  // }
 
   return (
     <>
       <form onSubmit={onSubmit}>
-        <input type="text" onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <input type="text" name="email" onChange={handleChange} />
+        <input type="password" name="password" onChange={handleChange} />
         <button type="submit">Sign In</button>
       </form>
     </>
