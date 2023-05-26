bd5bbe79d31eef837531638fac18349c8458c600
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-26 22:04:32 +0300

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,18 @@
+// "use client"
+
 import LoginForm from "@/components/LoginForm/LoginForm";
 import Link from "next/link";
+// import { useSession } from "next-auth/react";
+// import { redirect, usePathname } from "next/navigation";
+
 export default function LoginPage() {
+  // const pathname = usePathname();
+  // const session = useSession();
+
+  // if (pathname === "/login" && session) {
+  //   redirect("/home");
+  // }
+
   return (
     <>
       <Link href="/">HOME</Link>
