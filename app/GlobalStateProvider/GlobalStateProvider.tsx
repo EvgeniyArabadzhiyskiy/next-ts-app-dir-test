ffe5c4b69263e7daede85949bdac420c440bf947
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-26 22:04:26 +0300

    Update GlobalStateProvider.tsx

--- app/GlobalStateProvider/GlobalStateProvider.tsx
+++ app/GlobalStateProvider/GlobalStateProvider.tsx
@@ -19,10 +19,6 @@ export const getCurrentUser = async () => {
     },
   };
 
-  if (!authToken) {
-    return;
-  }
-  
   const resFetch = await fetch(`${BASE_URL}${USER_CURRENT}`, options);
   const user = (await resFetch.json()) as IUser;
 
@@ -44,38 +40,54 @@ interface IProps {
   user: IUser;
 }
 
-const privateRoutes = ["/", "/about", "/hydrate", "/pokemons"];
-const limitedRoutes = ["/login", "/register"];
+const privateRoutes = ["/about", "/hydrate", "/pokemons"];
+const limitedRoutes = ["/home", "/register"];
 
 function GlobalStateProvider({ children, isLoggedIn = false, user }: IProps) {
   const pathname = usePathname();
+  const { authToken } = parseCookies();
+  // console.log("GlobalStateProvider  authToken:", !!authToken);
 
-  const [globalData, setGlobalData] = useState<string>("Djon");
   const [userData, setUserData] = useState<IUser>(user);
 
-  // const { data, isLoading } = useQuery({
-  //   queryKey: ["currentUser"],
-  //   queryFn: getCurrentUser,
-  //   staleTime: Infinity,
-  // });
+  const { data, isFetched, isLoading, isFetching, isFetchedAfterMount } =
+    useQuery({
+      queryKey: ["currentUser"],
+      queryFn: getCurrentUser,
+      staleTime: Infinity,
+      enabled: !!authToken,
+    });
+  // console.log("GlobalStateProvider  data:", !data);
+  // console.log("isFetchedAfterMount  :", isFetching);
+  // console.log("GlobalStateProvider  isFetching:", isFetching);
+  // console.log("GlobalStateProvider  isLoading:", isLoading );
+  // console.log("GlobalStateProvider  data:", !!data?.email);
+
+  const fff = !!data?.email;
 
   return (
-    <StateContext.Provider
-      value={{ globalData, setGlobalData, isLoggedIn, userData, setUserData }}
-    >
+    <>
+      {/* <StateContext.Provider value={{ isLoggedIn, userData, setUserData }}> */}
       {false ? (
-        <h1>User Loading ...</h1>
+        <>
+          <h1>User Loading ...</h1>
+          <h1>{JSON.stringify(fff)}</h1>
+        </>
       ) : (
-        <ProtectedRoutes
-          pathname={pathname}
-          isLoggedIn={true}
-          privateRoutes={privateRoutes}
-          limitedRoutes={limitedRoutes}
-        >
-          {children}
-        </ProtectedRoutes>
+        <>
+          <ProtectedRoutes
+            pathname={pathname}
+            isLoggedIn={fff}
+            privateRoutes={privateRoutes}
+            limitedRoutes={limitedRoutes}
+          >
+            <h1>{JSON.stringify(!!data?.email)}</h1>
+            {children}
+          </ProtectedRoutes>
+        </>
       )}
-    </StateContext.Provider>
+      {/* </StateContext.Provider> */}
+    </>
   );
 }
 
