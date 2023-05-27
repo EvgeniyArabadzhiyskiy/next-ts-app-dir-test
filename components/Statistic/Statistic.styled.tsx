1cee3c4b970aada176c7486f7baadd5583dc6cf0
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-27 16:51:58 +0300

    Create Statistic.styled.tsx

---
+++ components/Statistic/Statistic.styled.tsx
@@ -0,0 +1,21 @@
+"use client"
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 30px;
+  color: #0674d4;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  width: 200px;
+  background: #11b843;
+`;
+
+
+export const HomeWrapper = styled.div`
+  background: #4a518b;
+`;
