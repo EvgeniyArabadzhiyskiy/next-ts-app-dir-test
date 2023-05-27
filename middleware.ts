e438d9e98d88ae83eecdc0f1cfcef58ea2c5ccb2
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-27 16:50:45 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,43 +1,44 @@
-// export { default } from "next-auth/middleware";
+export { default } from "next-auth/middleware";
 
-// export const config = {
-//   matcher: ["/home", "/about"],
-// };
+export const config = {
+  matcher: ["/hydrate", "/about"],
+};
 
 // =================================================================
 
-import { NextResponse } from "next/server";
-import { withAuth } from "next-auth/middleware";
+// import { NextResponse } from "next/server";
+// import { withAuth } from "next-auth/middleware";
 
-export default withAuth(
-  function middleware(req) {
-    const isLoggedIn = !!req.nextauth.token;
-    console.log("========================", isLoggedIn);
+// export default withAuth(
+//   function middleware(req) {
+//     const isLoggedIn = !!req.nextauth.token;
+//     console.log("========================", isLoggedIn);
 
-    // if (!isLoggedIn && req.nextUrl.pathname.startsWith("/about")) {
-    //   return NextResponse.redirect(new URL("/login", req.url));
-    // }
+//     // if (!isLoggedIn && req.nextUrl.pathname.startsWith("/about")) {
+//     //   return NextResponse.redirect(new URL("/login", req.url));
+//     // }
 
-    // if (isLoggedIn && req.nextUrl.pathname.startsWith("/login")) {
-    //   return NextResponse.redirect(new URL("/home", req.url));
-    // }
-  },
+//     // if (isLoggedIn && req.nextUrl.pathname.startsWith("/login")) {
+//     //   return NextResponse.redirect(new URL("/home", req.url));
+//     // }
+//   },
 
-  {
-    callbacks: {
-      authorized: ({ token, req }) => {
-        return !!token
+//   {
+//     callbacks: {
+//       authorized: ({ token, req }) => {
+//         return !!token
 
-        return false;
-      },
-    },
-  }
-);
+//         return false;
+//       },
+//     },
+//   }
+// );
 
-export const config = { matcher: [
-  // "/about",
-//  "/login"
-] };
+// export const config = { matcher: [
+//   "/about",
+//   '/hydrate',
+// //  "/login"
+// ] };
 
 // ========= Авторизация на сервере и приват редирект =================
 // import { NextResponse } from "next/server";
