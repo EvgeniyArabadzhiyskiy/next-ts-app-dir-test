97e1a06a61bfef09851dbf2460f12f94aa1ce21c
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-17 22:39:33 +0300

    Update GlobalStateProvider.tsx

--- app/GlobalStateProvider/GlobalStateProvider.tsx
+++ app/GlobalStateProvider/GlobalStateProvider.tsx
@@ -2,30 +2,11 @@
 
 import ProtectedRoutes from "@/components/ProtectedRoutes/ProtectedRoutes";
 import { useQuery } from "@tanstack/react-query";
-import { usePathname } from "next/navigation";
+import axios from "axios";
+import Link from "next/link";
+import { redirect, usePathname } from "next/navigation";
 import { parseCookies } from "nookies";
 import React, { createContext, useEffect, useState } from "react";
-
-export const getCurrentUser = async () => {
-  const BASE_URL = "https://wallet-backend-xmk0.onrender.com/api";
-  const USER_CURRENT = "/users/current";
-
-  const { authToken } = parseCookies();
-
-  const options = {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-    },
-  };
-
-  const resFetch = await fetch(`${BASE_URL}${USER_CURRENT}`, options);
-  const user = (await resFetch.json()) as IUser;
-
-  return user;
-};
-
-export const StateContext = createContext<any | undefined>(undefined);
 
 interface IUser {
   email?: string;
@@ -43,26 +24,76 @@ interface IProps {
 const privateRoutes = ["/about", "/hydrate", "/pokemons"];
 const limitedRoutes = ["/home", "/register"];
 
+export const getCurrentUser = async () => {
+  const BASE_URL = "https://wallet-backend-xmk0.onrender.com/api";
+  const USER_CURRENT = "/users/current";
+
+  const { authToken } = parseCookies();
+
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+    },
+  };
+
+  try {
+    // const res = await fetch(`${BASE_URL}${USER_CURRENT}`, options);
+
+    // if (!res.ok) {
+    //   throw new Error("NEW User  Unauthorized");
+    // }
+
+    // const user = (await res.json()) as IUser;
+    // return user;
+
+    const { data } = await axios(`${BASE_URL}${USER_CURRENT}`, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
+    // console.log("getCurrentUser  data:", data);
+    return data;
+  } catch (error) {
+    // console.log("getCurrentUser  error:", error);
+    throw error;
+  }
+};
+
+// if (user.message === "Unauthorized") {
+//   throw new Error("User Unauthorized");
+// }
+
+// export const StateContext = createContext<any | undefined>(undefined);
+
 function GlobalStateProvider({ children, isLoggedIn = false, user }: IProps) {
   // const pathname = usePathname();
-  // const { authToken } = parseCookies();
-  // console.log("GlobalStateProvider  authToken:", !!authToken);
+  const { authToken } = parseCookies();
 
-  
+  // getCurrentUser().then(data => {
+  //   console.log("DATA_USER", data);
+  // }).catch(err => {
+  //   console.log("getCurrentUser  err:", err);
+  // })
+
   // const [userData, setUserData] = useState<IUser>(user);
 
-
-
-  // const { data, isFetched, isLoading, isFetching, isFetchedAfterMount } =
-  // useQuery({
+  // const { data, error, isError, isFetching } = useQuery({
   //   queryKey: ["currentUser"],
   //   queryFn: getCurrentUser,
   //   staleTime: Infinity,
-  //   // enabled: !!authToken,
+  //   retry: false,
+  //   // retryDelay: 10000,
+  //   // retryOnMount: false,
+  //   onError: (err) => {
+  //     // console.log("GlobalStateProvider  err:", err);
+  //   },
   // });
+  // console.log("GlobalStateProvider  error:", error);
+  // console.log("GlobalStateProvider  data:", data);
 
+  // const isLogge = !!data?.email
 
-  // console.log("GlobalStateProvider  data:", !data);
   // console.log("isFetchedAfterMount  :", isFetching);
   // console.log("GlobalStateProvider  isFetching:", isFetching);
   // console.log("GlobalStateProvider  isLoading:", isLoading );
@@ -70,18 +101,28 @@ function GlobalStateProvider({ children, isLoggedIn = false, user }: IProps) {
 
   // const fff = !!data?.email;
 
+  // if (isFetching) {
+  //   // redirect('/login')
+  //   return <>
+  //   <h1>Loading Auth..</h1>
+  //   </>
+  // }
+
+  // if (isError) {
+  //   // redirect('/home')
+  //   // return <>
+  //   // <Link href='/login'>Login</Link>
+  //   // <h1>Error Auth</h1>
+  //   // </>
+  // }
+
   return (
     <>
       {/* <StateContext.Provider value={{ isLoggedIn, userData, setUserData }}> */}
-      {(false) ? (
-        <>
-          <h1>User Loading ...</h1>
-          {/* <h1>{JSON.stringify(fff)}</h1> */}
-        </>
-      ) : (
+      {
         <>
           <ProtectedRoutes
-            pathname={'/'}
+            pathname={"/"}
             isLoggedIn={true}
             privateRoutes={privateRoutes}
             limitedRoutes={limitedRoutes}
@@ -90,7 +131,7 @@ function GlobalStateProvider({ children, isLoggedIn = false, user }: IProps) {
             {children}
           </ProtectedRoutes>
         </>
-      )}
+      }
       {/* </StateContext.Provider> */}
     </>
   );
