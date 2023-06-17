a55e0db87ed48987469eb1f1544ac7042996ef46
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-17 22:39:35 +0300

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,8 +7,6 @@
 // } from "@/redux/pokemonApi";
 // import Link from "next/link";
 
-
-
 // interface IPokemon {
 //   name: string;
 //   url: string;
@@ -43,32 +41,73 @@
 
 import Link from "next/link";
 import Statistic from "@/components/Statistic/Statistic";
+import stl from "./Myhome.module.css";
+import { getUser } from "@/helpers/getUser";
+import { redirect } from "next/navigation";
+import Header from "@/components/Header/Header";
+
+
+import getQueryClient from "@/react-query/getQueryClient";
+import { Hydrate, dehydrate } from "@tanstack/react-query";
+import { cookies } from "next/headers";
 // import {
 //   getPokemonList,
 //   getRunningQueriesThunk,
 //   useGetPokemonListQuery,
 // } from "@/redux/pokemonApi";
 
-
 interface IPokemon {
   name: string;
   url: string;
 }
 
-export default function HomePage({params}: any) {
+interface UserData {
+  email: string;
+  firstName: string;
+  balance: number;
+
+}
+
+export default async function HomePage({ params }: any) {
+  const cookieStore = cookies();
+  const authToken = cookieStore.get("authToken")?.value;
+  
+  const queryUserData = await  getUser(authToken)
+  // console.log("HomePage  queryUserData:>>>>>>>>>>>>>>>>>>>>>>>>>>>", queryUserData);
+
 
   // const { data: pokemons, isLoading } = useGetPokemonListQuery(null);
   // console.log("HomePage  pokemons:", pokemons);
 
-  console.log("Home Page Client");
   
 
-  return (
-    <>
-      <h1>HOME PAGE</h1>
-      <Link href="/">HOME</Link>
-      <Statistic />
 
+  // const queryClient = getQueryClient();
+  // await queryClient.prefetchQuery(["User"], getUser);
+  // const dehydratedState = dehydrate(queryClient);
+
+  // const queryUserData = queryClient.getQueryData<Partial<UserData>>(["User"]);
+  // console.log("HomePage  queryUserData:<<<<<<<<<<<<<<>>>>>>>>>>>>>>", queryUserData);
+
+  const isLoggedIn = !!queryUserData?.email;
+
+
+  // if (isLoggedIn) {
+  //   redirect("/login");
+  // }
+
+  return (
+    // <Hydrate state={dehydratedState}>
+    <>
+    <Header
+      currentUser={queryUserData}
+    />
+      <div className={stl.container}>
+        <h1>HOME PAGE</h1>
+        <Link href="/">HOME</Link>
+        <h2>Container</h2>
+      </div>
+      ;{/* <Statistic /> */}
       {/* <ul>
         {pokemons &&
           pokemons.map((pokemon: IPokemon) => {
@@ -80,5 +119,6 @@ export default function HomePage({params}: any) {
           })}
       </ul> */}
     </>
+    // </Hydrate>
   );
 }
